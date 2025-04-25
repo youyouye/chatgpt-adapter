f387d0b928dadac0bf608f914c6d444f7689a4c8
panxuezhi <panxuezhi@bytedance.com>
2025-04-26 00:37:52 +0800

    fix

--- relay/llm/cursor/message.go
+++ relay/llm/cursor/message.go
@@ -126,10 +126,6 @@ func waitResponse(ctx *gin.Context, r *http.Response, sse bool) (content string)
 			continue
 		}
 
-		if !scanner.Scan() {
-			break
-		}
-
 		chunk := scanner.Bytes()
 
 		if len(chunk) == 0 {
